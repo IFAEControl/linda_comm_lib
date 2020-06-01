9f69cd39992bc358d5b552c866d2210fe1eb4857
David Roman <droman@ifae.es>
2020-06-01 20:01:58 +0200

    .

--- linda-lib/linda-lib.cpp
+++ linda-lib/linda-lib.cpp
@@ -1,9 +1,29 @@
 // linda-lib.cpp : Define las funciones de la biblioteca estática.
 //
 
+#include <iostream>
+
 #include <Poco/Net/DNS.h>
+#include <Poco/Net/SocketAddress.h>
+#include <Poco/Net/StreamSocket.h>
+#include <Poco/Net/SocketStream.h>
+#include <Poco/StreamCopier.h>
 
 // TODO: Ejemplo de una función de biblioteca
 void fnlindalib()
 {
+}
+
+int test() {
+	Poco::Net::SocketAddress sa("www.google.com", 80);
+	Poco::Net::StreamSocket socket(sa);
+	Poco::Net::SocketStream str(socket);
+	
+	str << "GET / HTTP/1.1\r\n"
+		"Host: www.google.com\r\n"
+		"\r\n";
+	str.flush();
+	
+	Poco::StreamCopier::copyStream(str, std::cout);
+	return 0;
 }
