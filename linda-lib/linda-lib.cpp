6963562b6affc783e157bd09663afb504d630173
David Roman <droman@ifae.es>
2020-06-14 12:53:07 +0200

    fix cmake

--- linda-lib/linda-lib.cpp
+++ linda-lib/linda-lib.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
 
-#include <Poco/Net/DNS.h>
-#include <Poco/Net/SocketAddress.h>
-#include <Poco/Net/StreamSocket.h>
-#include <Poco/Net/SocketStream.h>
-#include <Poco/StreamCopier.h>
-
 #include "linda-lib.hpp"
 
 // TODO: Ejemplo de una función de biblioteca
@@ -14,15 +8,5 @@ void fnlindalib()
 }
 
 int test() {
-	Poco::Net::SocketAddress sa("www.google.com", 80);
-	Poco::Net::StreamSocket socket(sa);
-	Poco::Net::SocketStream str(socket);
-	
-	str << "GET / HTTP/1.1\r\n"
-		"Host: www.google.com\r\n"
-		"\r\n";
-	str.flush();
-	
-	Poco::StreamCopier::copyStream(str, std::cout);
 	return 0;
 }
