d513f939e32e5f4d3cbfdcbab08fd16e3c81ecf3
David Roman <droman@ifae.es>
2020-08-03 12:58:05 +0200

    sockets example

--- linda-lib/linda-lib.cpp
+++ linda-lib/linda-lib.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 
-#include <Poco/Net/DNS.h>
+#include <Poco/Net/SocketAddress.h>
+#include <Poco/Net/StreamSocket.h>
+#include <Poco/Net/SocketStream.h>
+#include <Poco/StreamCopier.h>
 
 #include "linda-lib.hpp"
 
-using Poco::Net::DNS;
 using Poco::Net::IPAddress;
-using Poco::Net::HostEntry;
 
 // TODO: Ejemplo de una función de biblioteca
 void fnlindalib()
 {
-  const HostEntry& entry = DNS::hostByName("www.appinf.com");
+  //const HostEntry& entry = DNS::hostByName("www.appinf.com");
 }
 
 int test() {
+  Poco::Net::SocketAddress sa("127.0.0.1", 8080);
+  Poco::Net::StreamSocket socket(sa);
+  Poco::Net::SocketStream str(socket);
+  str << "GET / HTTP/1.1\r\n"
+         "Host: www.appinf.com\r\n"
+         "\r\n";
+
+  str.flush();
+  Poco::StreamCopier::copyStream(str, std::cout);
 	return 0;
 }
