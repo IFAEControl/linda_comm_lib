ccae4c719808ff23af0d646cb3f71630360466f3
David Roman <droman@ifae.es>
2020-08-03 12:27:09 +0200

    poco test

--- linda-lib/linda-lib.cpp
+++ linda-lib/linda-lib.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 
+#include <Poco/Net/DNS.h>
+
 #include "linda-lib.hpp"
+
+using Poco::Net::DNS;
+using Poco::Net::IPAddress;
+using Poco::Net::HostEntry;
 
 // TODO: Ejemplo de una función de biblioteca
 void fnlindalib()
 {
+  const HostEntry& entry = DNS::hostByName("www.appinf.com");
 }
 
 int test() {
