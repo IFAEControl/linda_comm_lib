8fae19420a3ac45e2ee83d66ffcadb32d44b4159
David Roman <droman@ifae.es>
2020-08-03 15:59:12 +0200

    fix build, hopefully

--- linda-lib/linda-lib.cpp
+++ linda-lib/linda-lib.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "Poco/Net/SocketAddress.h"
+#include <Poco/Net/SocketAddress.h>
 #include <Poco/Net/StreamSocket.h>
 #include <Poco/Net/SocketStream.h>
 #include <Poco/StreamCopier.h>
