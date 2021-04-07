e4d663d21479c9b57f66ea4dc67a68daa8178710
David Roman <droman@ifae.es>
2021-04-07 18:45:22 +0200

    add cli

--- linda-lib/test_client.cpp
+++ linda-lib/test_client.cpp
@@ -1,18 +1,70 @@
 #include <iostream>
 
+#include <cli/cli.h>
+#include <cli/clifilesession.h>
+
 #include "linda-lib.hpp"
 
+using namespace cli;
+
 int main() {
-    int n;
-    std::cin >> n;
+    unsigned data[14400];
     InitCommunication("172.16.17.94", 32000, 32001);
-    unsigned id[30];
-    FullArrayReadEricaID(id, 5);
-    /*unsigned t;
-	ReadTemperature(&t, 1);*/
-	//std::cout << t << std::endl;
-    /*unsigned temp = 0;
-    ReadTemperature(&temp);
-    std::cout << temp << std::endl;*/
-    //readReg();
+
+    auto rootMenu = std::make_unique<Menu>("linda");
+
+    rootMenu -> Insert(
+            "start", [&](std::ostream& out, int chip) { 
+                AcqInfo info{10,10,10,true,true,false};
+                ACQuisitionCont(info, data, 1<<chip);
+                out << "done\n"; 
+        },
+            "Start acquisition" 
+    );
+
+    rootMenu -> Insert(
+            "pop_frame", [&](std::ostream& out) {
+                PopFrame(data);
+                out << "done\n"; 
+        },
+            "Pop frame" 
+    );
+
+    rootMenu -> Insert(
+            "stop", [&](std::ostream& out) { 
+                ACQuisitionStop();
+                out << "done\n"; 
+        },
+            "Stop acquisition" 
+    );
+
+    rootMenu -> Insert(
+            "read_id", [&](std::ostream& out, int chip) { 
+                unsigned id;
+                ReadEricaID(&id, 1<<chip);
+                out << "Chip id: " << id << "\n"; 
+        },
+            "Read chip id" 
+    );
+
+    rootMenu -> Insert(
+            "read_temp", [&](std::ostream& out, int chip) { 
+                unsigned temp;
+                ReadTemperature(&temp, 1<<chip);
+                out << "Temperature: " << temp << "\n"; 
+        },
+            "Read temperature" 
+    );
+
+    rootMenu -> Insert(
+            "reset", [&](std::ostream& out) { 
+                CameraReset();
+                out << "done \n"; 
+        },
+            "Reset camera" 
+    );
+    Cli cli(std::move(rootMenu));
+    CliFileSession input(cli);
+    SetColor();
+    input.Start();
 }
