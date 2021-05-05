f38452e2796e128e5cd59e5ae55fd6fe3ecb06c5
David Roman <droman@ifae.es>
2021-05-05 15:18:28 +0200

    reestructure cmake

---
+++ CMake/Conan.cmake
@@ -0,0 +1,25 @@
+# Download automatically, you can also just copy the conan.cmake file
+if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+    message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake"
+            "${CMAKE_BINARY_DIR}/conan.cmake")
+endif()
+
+include(${CMAKE_BINARY_DIR}/conan.cmake)
+conan_check(VERSION 1.24.1 REQUIRED)
+
+conan_add_remote(
+        NAME bincrafters
+        URL https://api.bintray.com/conan/bincrafters/public-conan
+)
+
+conan_cmake_run(
+        CONANFILE conanfile.txt
+        BASIC_SETUP
+        CMAKE_TARGETS
+        BUILD_TYPE "Release"
+        BUILD missing
+)
+
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()
