0e81e393210e3b187dbb0b4d79ea6588c7488937
David Roman <droman@ifae.es>
2021-07-02 00:32:18 +0200

    remove old shit

--- lib/networking.cpp
+++ lib/networking.cpp
@@ -45,15 +45,6 @@ DataReceiver::DataReceiver(const std::string& ip, unsigned short p) :
     _sa{ip, p}
 {}
 
-void DataReceiver::configure(const std::string& ip, unsigned short port) {
-    _sa = Poco::Net::SocketAddress(ip, port);
-    connect();
-}
-
-//DataReceiver::~DataReceiver() {
- //   _reader.~thread();
-//}
-
 void DataReceiver::initThread() {
     connect();
     if(!_thread_running) {
